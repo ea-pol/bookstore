240c572e316b464dfd9501adc7342016334bbdfb
Eugene <polevikov.e@yandex.ru>
2023-04-16 06:57:18 +0300

    Setup hibernate configuration

---
+++ server/core/src/main/java/org/eapol/bookstore/config/HibernateConfig.java
@@ -0,0 +1,66 @@
+package org.eapol.bookstore.config;
+
+import org.eapol.bookstore.author.Author;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Environment;
+import org.hibernate.dialect.PostgreSQLDialect;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+import javax.sql.DataSource;
+import java.util.Properties;
+
+@Configuration
+public class HibernateConfig {
+
+  @Value("${database.url}")
+  private String databaseUrl;
+
+  @Value("${database.username}")
+  private String databaseUsername;
+
+  @Value("${database.password}")
+  private String databasePassword;
+
+  @Value("${hibernate.hbm2ddl.auto}")
+  private String hbm2ddlAuto;
+
+  @Bean
+  public DataSource dataSource() {
+    return new DriverManagerDataSource(databaseUrl, databaseUsername, databasePassword);
+  }
+
+  @Bean
+  public LocalSessionFactoryBean localSessionFactoryBean() {
+    LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+
+    sessionFactory.setDataSource(dataSource());
+    sessionFactory.setAnnotatedClasses(Author.class);
+    sessionFactory.setHibernateProperties(hibernateProperties());
+
+    return sessionFactory;
+  }
+
+  @Bean
+  public SessionFactory sessionFactory(LocalSessionFactoryBean localSessionFactoryBean) {
+    return localSessionFactoryBean.getObject();
+  }
+
+  @Bean
+  public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+    return new HibernateTransactionManager(sessionFactory);
+  }
+
+  private Properties hibernateProperties() {
+    Properties hibernateProperties = new Properties();
+
+    hibernateProperties.put(Environment.DIALECT, PostgreSQLDialect.class.getName());
+    hibernateProperties.put(Environment.HBM2DDL_AUTO, hbm2ddlAuto);
+
+    return hibernateProperties;
+  }
+}
