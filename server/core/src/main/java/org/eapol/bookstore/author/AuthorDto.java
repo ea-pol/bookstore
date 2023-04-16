fbc827041304a4d32a5a461cdaa2a36f7b529c6b
Eugene <polevikov.e@yandex.ru>
2023-04-16 07:36:49 +0300

    Add author dto, update author resource

---
+++ server/core/src/main/java/org/eapol/bookstore/author/AuthorDto.java
@@ -0,0 +1,27 @@
+package org.eapol.bookstore.author;
+
+public class AuthorDto {
+  private String firstName;
+  private String lastName;
+
+  public AuthorDto(String firstName, String lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+}
--- server/core/src/main/java/org/eapol/bookstore/author/AuthorResource.java
+++ server/core/src/main/java/org/eapol/bookstore/author/AuthorResource.java
@@ -1,8 +1,18 @@
 package org.eapol.bookstore.author;
 
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
 
 @Path("/api/authors")
 public class AuthorResource {
 
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public List<AuthorDto> authors() {
+    return null;
+  }
 }
