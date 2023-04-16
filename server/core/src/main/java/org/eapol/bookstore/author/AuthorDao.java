d2fbb019520d4a24f010a19d545daf82256ad980
Eugene <polevikov.e@yandex.ru>
2023-04-16 07:44:18 +0300

    Setup author dao template

--- server/core/src/main/java/org/eapol/bookstore/author/AuthorDao.java
+++ server/core/src/main/java/org/eapol/bookstore/author/AuthorDao.java
@@ -1,4 +1,20 @@
 package org.eapol.bookstore.author;
 
+import jakarta.inject.Inject;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class AuthorDao {
+  private final SessionFactory sessionFactory;
+
+  @Inject
+  public AuthorDao(SessionFactory sessionFactory) {
+    this.sessionFactory = sessionFactory;
+  }
+
+  private Session session() {
+    return sessionFactory.getCurrentSession();
+  }
 }
