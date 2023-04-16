d2b8ee8b64cceab24927fa407e1d7be89b86305d
Eugene <polevikov.e@yandex.ru>
2023-04-16 07:04:15 +0300

    Define 'Author' entity

--- server/core/src/main/java/org/eapol/bookstore/author/Author.java
+++ server/core/src/main/java/org/eapol/bookstore/author/Author.java
@@ -1,4 +1,47 @@
 package org.eapol.bookstore.author;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "author")
 public class Author {
+  @Id
+  @GeneratedValue(strategy = GenerationType.SEQUENCE)
+  @Column(name = "author_id")
+  private Long authorId;
+
+  @Column(name = "first_name")
+  private String firstName;
+
+  @Column(name = "last_name")
+  private String lastName;
+
+  public void setAuthorId(Long authorId) {
+    this.authorId = authorId;
+  }
+
+  public Long getAuthorId() {
+    return authorId;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
 }
