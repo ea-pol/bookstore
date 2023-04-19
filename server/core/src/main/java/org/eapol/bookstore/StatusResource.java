4ae685bea1f6b0937d06420067c4c2f7d0404197
Eugene <polevikov.e@yandex.ru>
2023-04-19 22:50:04 +0300

    Refactor status resource

--- server/core/src/main/java/org/eapol/bookstore/StatusResource.java
+++ server/core/src/main/java/org/eapol/bookstore/StatusResource.java
@@ -2,14 +2,15 @@ package org.eapol.bookstore;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
 
 @Path("/api")
 public class StatusResource {
 
   @GET
   @Path("/status")
-  public String status() {
-    return "I'm alive!";
+  public Response status() {
+    return Response.ok().build();
   }
 
 }
