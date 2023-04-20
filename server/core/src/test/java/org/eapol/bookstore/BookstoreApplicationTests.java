2b5124b7c04a6d2aae4f39213724a2fd5dcce957
Eugene <polevikov.e@yandex.ru>
2023-04-20 20:04:43 +0300

    Setup initial test env

--- server/core/src/test/java/org/eapol/bookstore/BookstoreApplicationTests.java
+++ server/core/src/test/java/org/eapol/bookstore/BookstoreApplicationTests.java
@@ -1,13 +1,45 @@
 package org.eapol.bookstore;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
-@SpringBootTest
-class BookstoreApplicationTests {
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+public class BookstoreApplicationTests {
+  private static final OkHttpClient client = new OkHttpClient();
+  private static final String HOST = "http://localhost:8080";
 
   @Test
-  void contextLoads() {
+  public void dummyTest() {
+    assertTrue(true);
   }
 
+  @Test
+  public void testStatusResource() throws IOException, InterruptedException {
+    Thread thread = new Thread(() -> {
+      try {
+        BookstoreApplication.main(new String[]{});
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    });
+
+    thread.start();
+
+    Thread.sleep(10_000);
+
+    Request request = new Request.Builder()
+      .url(HOST + "/api/status")
+      .build();
+
+    Response response = client.newCall(request).execute();
+    System.out.println(response.code());
+
+    assertEquals(response.code(), 200);
+  }
 }
