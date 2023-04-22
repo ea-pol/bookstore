86d78922e4b314a4e7e87fd42325e036e5507f8e
Eugene <polevikov.e@yandex.ru>
2023-04-22 23:18:15 +0300

    More UI improvements

--- client/books.js
+++ client/books.js
@@ -4,37 +4,37 @@ function fetchBooks() {
   var book1 = {
     id: 1,
     author: "Ray Bradbury",
-    title: "Title 1",
-    firstSentence: "First sentence 1",
-    price: "42",
-    amount: "92"
+    title: "Fahrenheit 451",
+    firstSentence: "It was a special pleasure to see things eaten, to see things blackened and changed..",
+    price: 42,
+    amount: 92
   };
 
   var book2 = {
     id: 2,
     author: "Ray Bradbury",
-    title: "Title 2",
-    firstSentence: "First sentence 2",
-    price: "42",
-    amount: "92"
+    title: "Dandelion Wine",
+    firstSentence: "It was a quiet morning, the town covered over with darkness and at ease in bed..",
+    price: 42,
+    amount: 92
   };
 
   var book3 = {
     id: 3,
-    author: "Ray Bradbury",
-    title: "Title 3",
-    firstSentence: "First sentence 3",
-    price: "42",
-    amount: "92"
+    author: "Leo Tolstoy",
+    title: "Anna Karenina",
+    firstSentence: "Happy families are all alike; every unhappy family is unhappy in its own way..",
+    price: 42,
+    amount: 92
   }
 
   var book4 = {
     id: 4,
-    author: "Ray Bradbury",
-    title: "Title 4",
-    firstSentence: "First sentence 4",
-    price: "42",
-    amount: "92"
+    author: "Leo Tolstoy",
+    title: "War and Peace",
+    firstSentence: "It was in July, 1805, and the speaker was the well-known Anna Pavlovna Scherer, maid of honor and favorite of the Empress Marya Fedorovna..",
+    price: 42,
+    amount: 92
   }
 
   books.push(book1);
@@ -59,15 +59,21 @@ function displayBooks() {
     var firstSentence = document.createElement("div");
     firstSentence.innerHTML += books[i].firstSentence;
 
+    var bookContent = document.createElement("div");
+    bookContent.appendChild(book);
+    bookContent.appendChild(firstSentence);
+    bookContent.className = "list-item-elem";
+
     var removeButton = document.createElement("div");
     removeButton.innerHTML += "x"
     removeButton.addEventListener("click", removeBook);
     removeButton.setAttribute("data-book-id", books[i].id);
+    removeButton.className = "list-item-elem list-item-remove-button";
 
     var booksListItem = document.createElement("div");
-    booksListItem.appendChild(book);
-    booksListItem.appendChild(firstSentence);
-    booksListItem.appendChild(removeButton);
+    booksListItem.appendChild(bookContent);
+    // booksListItem.appendChild(removeButton);
+    booksListItem.className = "book-list-item";
 
     booksList.appendChild(booksListItem);
   }
